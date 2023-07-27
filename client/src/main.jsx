11252aa0c2042a5c2432057aa9cdff5b2184d732
Nirbhay Singh <81435375+Nirbhay09Singh@users.noreply.github.com>
2023-07-27 10:48:09 +0530

    Change to deploy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
      domain="dev-rxalu7cq3t4un0yl.us.auth0.com"
      clientId="ziHqRfeA3hkmAGVUeVXWBZxxmzfz1zCz"
      authorizationParams={{
-      redirect_uri: "http://localhost:5173"
+      redirect_uri: "https://royal-castle.vercel.app"
      }}
      audience='http://localhost:8000'
      scope="openid profile email"
